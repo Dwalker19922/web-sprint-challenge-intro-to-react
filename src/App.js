6c636d014a65e29fbe798cafa43ba45742308c05
Daniel Walker <dw765608@gmail.com>
2021-04-16 16:37:53 -0400

    Daniel-Walker-Flex

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from'axios'
+import Character from'./components/Character'
+
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  const [characters, setcharacters] = useState([]);
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+   useEffect(() => {
+       axios
+       .get("https://swapi.dev/api/people/")
+       .then(res => {setcharacters(res.data)})
+       
+    }
+   , ["https://swapi.dev/api/people/"]);
+  
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+       {characters.map(ele => {
+         console.log(characters)
+         return <Character characters={ele} key={ele.name}/>
+       }
+       )}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,49 @@
-// Write your Character component here
+import React,{useState,useEffect} from 'react'
+import styled from 'styled-components'
+const MyStyled = styled.div`
+.div {
+    display:flex;
+    flex-direction:column;
+    justify-content: space-around;
+    background-color:black;
+    width:20%;
+    margin: 0 auto;
+}
+.div p{
+    color:yellow;
+    font-style:italic;
+}
+.name{
+    font-style:italic;
+}
+button{
+    padding:1%;
+}
+`
+const show = ()=>{
+
+}
+const Character = (props) => { 
+const {characters} = props 
+console.log(characters.starships)
+return  (
+    <MyStyled>
+    <div className="name">
+        {console.log(characters.name)}
+        <h1>{characters.name}</h1>
+     </div>
+    <div className="div"> 
+        <p>Birth year: {characters.birth_year}</p>
+        <p>Eye color: {characters.eye_color}</p>
+        <p>Gender: {characters.gender}</p>
+        <p>Hair Color: {characters.hair_color}</p>
+        <p>Mass: {characters.mass}</p>
+        <p>Skin Color: {characters.skin_color}</p>
+    </div>
+    </MyStyled>
+       
+
+    
+)
+}
+export default Character
